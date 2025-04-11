98a1bf099647e5b212d62b0e3d489d1f8067b647
Undeciphered <arseneault3luc@gmail.com>
2025-04-11 14:04:15 -0300

    Assembled functions into tic_tac_toe class

--- Tic-tac-toe.cpp
+++ Tic-tac-toe.cpp
@@ -1,101 +1,139 @@
 #include <iostream>
 #include <array>
 
-char check_winner(char board[3][3]) {
-    for(int i = 0; i < 3; i++){
-        if(board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][2] != ' ') {
-            return board[i][0];   
-        }
-        if(board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[2][i] != ' ') {
-            return board[0][i];   
-        } 
-    }
-    if(board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[2][2] != ' ') {
-        return board[0][0];   
-    }
-    if(board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[2][0] != ' ') {
-        return board[0][2];   
-    } 
-    return ' ';
-}  
-
-bool is_draw(char board[3][3]) {
-    for(int i = 0; i < 3; i++) {
-        for(int j = 0; j < 3; j++) {
-            if(board[i][j] == ' ') {
-                return false;  
-            }
-        }
-    }
-    if(check_winner(board) == ' ') {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-int minimax(char board[3][3], bool ismaximizing) {
-    char winner = check_winner(board);
-    if(winner == 'X') {return +1;}
-    if(winner == 'O') {return -1;}
-    if(is_draw(board)) {return 0;}
-    
-    if(ismaximizing) {
-        int best_score{-1748};
-        for(int i = 0; i < 3; i++) {
-            for(int j = 0; j < 3; j++) {
-                if(board[i][j] == ' ') {
-                    board[i][j] = 'X';
-                    int score{minimax(board, false)};
-                    board[i][j] = ' ';
-                    best_score = std::max(best_score, score);
+class tic_tac_toe {
+    private:
+        char board[3][3];
+        
+    public:
+        tic_tac_toe() {
+            for(auto& row : board) {
+                for(char& cell : row) {
+                    cell = ' ';
                 }
             }
         }
-        return best_score;
-    } else {
-        int best_score{1748};
-        for(int i = 0; i < 3; i++) {
-            for(int j = 0; j < 3; j++) {
-                if(board[i][j] == ' ') {
-                    board[i][j] = 'O';
-                    int score{minimax(board, true)};
-                    board[i][j] = ' ';
-                    best_score = std::min(best_score, score);
+        
+        char check_winner() {
+            for(int i = 0; i < 3; i++){
+                if(board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][2] != ' ') {
+                    return board[i][0];   
+                }
+                if(board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[2][i] != ' ') {
+                    return board[0][i];   
+                } 
+            }
+            if(board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[2][2] != ' ') {
+                return board[0][0];   
+            }
+            if(board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[2][0] != ' ') {
+                return board[0][2];   
+            } 
+            return ' ';
+        }
+        
+        bool is_draw() {
+            for(int i = 0; i < 3; i++) {
+                for(int j = 0; j < 3; j++) {
+                    if(board[i][j] == ' ') {
+                        return false;  
+                    }
                 }
             }
+            if(check_winner() == ' ') {
+                return true;
+            } else {
+                return false;
+            }
+            return true;
         }
-        return best_score;  
-    } 
-}
-
-void minimax_best_move(char board[3][3]) {
-    int best_row{-1}, best_column{-1};
-    int best_score{-1748};
-    for(int i = 0; i < 3; i++) {
-        for(int j = 0; j < 3; j++) {
-            if(board[i][j] == ' ') {
-                board[i][j] = 'X';
-                int score{minimax(board, false)};
-                board[i][j] = ' ';
-                if(score > best_score) {
-                    best_score = score;
-                    best_row = i;
-                    best_column = j;
+        
+        int minimax(bool ismaximizing) {
+            char winner = check_winner();
+            if(winner == 'X') {return +1;}
+            if(winner == 'O') {return -1;}
+            if(is_draw()) {return 0;}
+            
+            if(ismaximizing) {
+                int best_score{-1748};
+                for(int i = 0; i < 3; i++) {
+                    for(int j = 0; j < 3; j++) {
+                        if(board[i][j] == ' ') {
+                            board[i][j] = 'X';
+                            int score{minimax(false)};
+                            board[i][j] = ' ';
+                            best_score = std::max(best_score, score);
+                        }
+                    }
                 }
+                return best_score;
+            } else {
+                int best_score{1748};
+                for(int i = 0; i < 3; i++) {
+                    for(int j = 0; j < 3; j++) {
+                        if(board[i][j] == ' ') {
+                            board[i][j] = 'O';
+                            int score{minimax(true)};
+                            board[i][j] = ' ';
+                            best_score = std::min(best_score, score);
+                        }
+                    }
+                }
+                return best_score;  
             }
         }
-    }
-    if(best_row != -1 && best_column != -1) {
-        board[best_row][best_column] = 'X';
-    }
-}
+        
+        void minimax_best_move() {
+            int best_row{-1}, best_column{-1};
+            int best_score{-1748};
+            for(int i = 0; i < 3; i++) {
+                for(int j = 0; j < 3; j++) {
+                    if(board[i][j] == ' ') {
+                        board[i][j] = 'X';
+                        int score{minimax(false)};
+                        board[i][j] = ' ';
+                        if(score > best_score) {
+                            best_score = score;
+                            best_row = i;
+                            best_column = j;
+                        }
+                        }
+                }
+            }
+            if(best_row != -1 && best_column != -1) {
+                board[best_row][best_column] = 'X';
+            }
+        }
+        
+        void print_board() {
+            for(int i = 0; i < 3; i++) {
+                for(int j = 0; j < 3; j++) {
+                   std::cout << board[i][j] << ' ';
+                }        
+                std::cout << '\n';
+            }
+        }
+                
+        void start() {
+            while(true) {
+                int row{0}, column{0};
+                minimax_best_move();
+                print_board();
+                if(check_winner() == 'X') {
+                std::cout << "X won";
+                break;
+                } else if(is_draw()) {
+                    std::cout << "its a tie";
+                    break;
+                }
+                std::cin >> row >> column;
+                std::cout << '\n';
+                board[row][column] = 'O';
+            }       
+        }
+};
 
 int main() {
-    char board[3][3] = {
-    { ' ', ' ', ' ' },
-    { ' ', ' ', ' ' },
-    { ' ', ' ', ' ' }};
-
-    return 0;
+    tic_tac_toe mygame;
+    mygame.start();
 }
