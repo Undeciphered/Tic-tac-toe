54baf6f19a1d9bc5dc24303d645e15df039b68a2
Undeciphered <arseneault3luc@gmail.com>
2025-04-11 13:22:53 -0300

    Fixed check_winner function
    check_winner would count rows of ' ' and output them as winer ignoring actual wins of X or O, check_winner now ignores wins of ' '.

--- Tic-tac-toe.cpp
+++ Tic-tac-toe.cpp
@@ -3,17 +3,17 @@
 
 char check_winner(char board[3][3]) {
     for(int i = 0; i < 3; i++){
-        if(board[i][0] == board[i][1] && board[i][1] == board[i][2]) {
+        if(board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][2] != ' ') {
             return board[i][0];   
         }
-        if(board[0][i] == board[1][i] && board[1][i] == board[2][i]) {
+        if(board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[2][i] != ' ') {
             return board[0][i];   
         } 
     }
-    if(board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
+    if(board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[2][2] != ' ') {
         return board[0][0];   
     }
-    if(board[0][2] == board[1][1] && board[1][1] == board[2][0]) {
+    if(board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[2][0] != ' ') {
         return board[0][2];   
     } 
     return ' ';
